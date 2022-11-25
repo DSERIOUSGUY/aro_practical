6ea43dd955ac0cf7859bf1e4bbd57dd8ae2e78b1
DSERIOUSGUY <utsavagarwal2014@gmail.com>
2022-11-25 01:26:37 +0000

    cleaning up code, adding animation to keep dumbell back, works(slow but works and is stable)

--- task3_2/task3_2_docking_template.py
+++ task3_2/task3_2_docking_template.py
@@ -100,94 +100,103 @@ def getReadyForTask():
     return tableId, cubeId, targetId
 
 
-def solution():
-    # TODO: Add your code here
-    goals = [[0.45, 0.15, 0.20], [0.45, 0.05, 0.27], [0.4, 0.15, 0.51]]
-    threshold = 0.1
-    goalOrient = [0, -1, 0]
-    for k in goals:
-        targetL = sim.inverseKinematics('LHAND', k, goalOrient
-                                        , 10, 1, 1e-3)
-        k[1] -= 0.2
-        targetR = sim.inverseKinematics('RHAND', k, [0, 1, 0]
-                                        , 10, 1, 1e-3)
-
-        for l in range(len(targetL)):
-            for m in range(3, 9):
-                if (sim.jointRotationAxis[sim.jointList[m]] @ np.array([0, 0, 1]) == 1) or sim.jointRotationAxis[
-                    sim.jointList[m]] @ np.array([1, 0, 0]) == 1:
-                    targetR[l][6 + m] = -1 * targetL[l][m]
-                else:
-                    targetR[l][6 + m] = targetL[l][m]
-        target = np.array([targetL[0]])
-        for i in range(len(targetL)):
-            target = np.append(target, np.array([np.hstack((targetL[i][0:9], targetR[i][9:15]))]), axis=0)
-            # target = list(targetL[i][0:9])+list(targetR[i][9:15])
-        # target = sim.inverseKinematics('RHAND', k, goalOrient
-        #                                , 10, 1, 1e-3)
-        print("_________target= ", k)
-        for j in target:
-            print("subtarget= ", sim.efForwardKinematics('RARM_JOINT5', j)[0])
-            for idi, i in enumerate(sim.jointList):
-                sim.target_pos[i] = j[idi]
-                sim.target_vel[i] = 0.05
-            q = np.array([])
-            for joint in sim.jointList:
-                q = np.append(q, np.array([sim.getJointPos(joint)]), axis=0)
-                # print(q)
-            counter = 0
-            while np.amax(np.absolute(q - j)) > threshold:
-                # while np.linalg.norm(
-                #        sim.efForwardKinematics('RARM_JOINT5', q)[0] - sim.efForwardKinematics('RARM_JOINT5', j)[
-                #            0]) > threshold:
-                sim.tick()
-                time.sleep(1 / 1000)
-                q = np.array([])
-                for joint in sim.jointList:
-                    q = np.append(q, np.array([sim.getJointPos(joint)]), axis=0)
-                    # print(q)
-                counter += 1
-                if counter >= 2000:
-                    print("____NOT REACHED_____", "position=", sim.getJointPosition('RARM_JOINT5'))
-                    print(list(sim.target_pos.values()) - q)
-
-                    break
-            print("subtarget reached=", sim.getJointPosition('RARM_JOINT5'), "orientation=",
-                  sim.getJointOrientation('RARM_JOINT5'))
-        print("target reached=", k, "position=", sim.getJointPosition('RARM_JOINT5'), "orientation=",
-              sim.getJointOrientation('RARM_JOINT5'))
-
-    # targetChest = sim.inverseKinematics('CHEST_JOINT0', sim.getJointPosition('CHEST_JOINT0'), [1, 1, 0], 10, 1,
-    #                                    threshold)
-    q = np.array([])
-    for joint in sim.jointList:
-        q = np.append(q, np.array([sim.getJointPos(joint)]), axis=0)
-    targetChest = np.insert(q[1:15], 0, np.deg2rad(55))
-    # targetChest[0] = np.deg2rad(45)
-
-    for idi, i in enumerate(sim.jointList):
-        sim.target_pos[i] = targetChest[idi]
-        sim.target_vel[i] = 0.05
-
-        # print(q)
-    counter = 0
-    while np.amax(np.absolute(q - targetChest)) > threshold:
-        # while np.linalg.norm(
-        #        sim.efForwardKinematics('RARM_JOINT5', q)[0] - sim.efForwardKinematics('RARM_JOINT5', j)[
-        #            0]) > threshold:
-        sim.tick()
-        time.sleep(1 / 1000)
+def move_with_q(target,threshold,vel):
+    for j in target:
+        for idi, i in enumerate(sim.jointList):
+            sim.target_pos[i] = j[idi]
+            sim.target_vel[i] = vel
         q = np.array([])
         for joint in sim.jointList:
             q = np.append(q, np.array([sim.getJointPos(joint)]), axis=0)
             # print(q)
-        counter += 1
-        if counter >= 2000:
-            print("____NOT REACHED_____", "position=", sim.getJointPosition('RARM_JOINT5'))
-            print(list(sim.target_pos.values()) - q)
+        counter = 0
+        while np.amax(np.absolute(q - j)) > threshold:
+            # while np.linalg.norm(
+            #        sim.efForwardKinematics('RARM_JOINT5', q)[0] - sim.efForwardKinematics('RARM_JOINT5', j)[
+            #            0]) > threshold:
+            sim.tick()
+            time.sleep(1 / 1000)
+            q = np.array([])
+            for joint in sim.jointList:
+                q = np.append(q, np.array([sim.getJointPos(joint)]), axis=0)
+                # print(q)
+            counter += 1
+            if counter >= 2000:
+                print("____NOT REACHED_____", "position=", sim.getJointPosition('RARM_JOINT5'))
+                print(list(sim.target_pos.values()) - q)
 
-            break
+                break
+        print("subtarget reached=", sim.getJointPosition('RARM_JOINT5'), "orientation=",
+                sim.getJointOrientation('RARM_JOINT5'))
 
+
+def move_arms_identical(k,goalOrient,separation=0.2):
+    targetL = sim.inverseKinematics('LHAND', k, goalOrient
+                                    , 10, 1, 1e-3)
+    k[1] -= separation
+    targetR = sim.inverseKinematics('RHAND', k, [0, 1, 0]
+                                    , 10, 1, 1e-3)
+
+    for l in range(len(targetL)):
+        for m in range(3, 9):
+            if (sim.jointRotationAxis[sim.jointList[m]] @ np.array([0, 0, 1]) == 1) or sim.jointRotationAxis[
+                sim.jointList[m]] @ np.array([1, 0, 0]) == 1:
+                targetR[l][6 + m] = -1 * targetL[l][m]
+            else:
+                targetR[l][6 + m] = targetL[l][m]
+    target = np.array([targetL[0]])
+    for i in range(len(targetL)):
+        target = np.append(target, np.array([np.hstack((targetL[i][0:9], targetR[i][9:15]))]), axis=0)
+
+    return target
+
+
+def solution():
+    # TODO: Add your code here
+
+
+    #move arms up
+    goals = [[0.40, 0.30, 0.25], [0.50, 0.07, 0.22], [0.45, 0.07, 0.40]]
+    threshold = [0.2,0.1,0.1,0.1]
+    goalOrient = [[0, 0, 1],[1, 0, 0],[1, 0, 0],[0, 1, 0]]
+    for k in range(len(goals)):
+        target = move_arms_identical(goals[k],goalOrient[k])
+        print("_________target= ", k)
+        move_with_q(target,threshold[k],0)
+        print("target reached=", k, "position=", sim.getJointPosition('RARM_JOINT5'), "orientation=",
+        sim.getJointOrientation('RARM_JOINT5'))
+        
+
+    #rotate chest
+    q = np.array([])
+    for joint in sim.jointList:
+        q = np.append(q, np.array([sim.getJointPos(joint)]), axis=0)
+    targetChest = np.insert(q[1:15], 0, np.deg2rad(55))
+
+    move_with_q([targetChest],0.1,0.05)
+
+    #move arms down
+    # curr_arm_pos = sim.getJointPosition('LARM_JOINT5')
+    # curr_arm_pos[2] = 0.18
+
+    goals = [[0.30482338, 0.35085965, 0.23],[0.1, 0.35, 0.23]]
+    goalOrient = [[0.54114996, 0.79068419, 0.28631317]]
+    separation = [0,0.5]
+
+    #only height decreases
+    
+ 
+    for k in range(len(goals)):
+        target = move_arms_identical(goals[k],goalOrient[k],separation[k])
+        print("_________target= ", k)
+        move_with_q(target,threshold[k],0)
+        print("target reached=", k, "position=", sim.getJointPosition('RARM_JOINT5'), "orientation=",
+        sim.getJointOrientation('RARM_JOINT5'))
+
+
+    print("***FINISHED***")
+    for i in range(10):
+        sim.tick()
 
 tableId, cubeId, targetId = getReadyForTask()
 solution()
